f4182a91bac50ac9704b91b2d3d5197e65092dd0
SGXaba <sgxaba7@gmail.com>
2022-09-18 08:07:06 -0700

    8-print_square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -2,18 +2,18 @@
 #include "main.h"
 
 /**
-*print_swuare - prints squares
+*print_square - prints squares
 *@size: parameter
 *Return: returns nothing
 */
 
 void print_square(int size)
 {
-	int incl, inc2;
+	int inc1, inc2;
 
 	if (size > 0)
 	{
-		for (inc1 = 0; inc1 < size; incl++)
+		for (inc1 = 0; inc1 < size; inc2++)
 		{
 			for (inc2 = 0; inc2 < (size - 1); inc2++)
 			{
