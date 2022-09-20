51a9038fd17188e9d6ecf08bd5f1a1d4db52084f
SGXaba <sgxaba7@gmail.com>
2022-09-19 23:19:17 -0700

    0-reset_to_98

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN.H
+#define MAIN.H
+
+/**
+* void prototypes()
+* int prototypes()
+* /
+
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strien(char *s);
+void _puts(char *src);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strepy(char *dest, char *src);
+int _atoi(char *str);
+
+#endif /*MAIN_H*/
