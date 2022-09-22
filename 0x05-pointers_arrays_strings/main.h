600d1851ba4bcea5c94ced9890f6e9c535dacc0b
SGXaba <sgxaba7@gmail.com>
2022-09-22 07:33:20 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,8 +8,8 @@
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-int _strien(char *s);
-void _puts(char *src);
+int _strlen(char *s);
+void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
