e8775f34f7dbff72b3bef10da936a27d3f56ffa3
SGXaba <sgxaba7@gmail.com>
2022-09-22 07:08:11 -0700

    reversing string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -14,10 +14,9 @@ void rev_string(char *s)
 
 	while (s[length1] != '\n')
 	length1++;
-	length2 = length1 - 1
+	length2 = length1 - 1;
 	for (i = 0; i < length1 / 2; i++)
 	{
-		"samalx" "xlamas"
 		tmp = s[i];
 		s[i] = s[length2];
 		s[length2] = tmp;
