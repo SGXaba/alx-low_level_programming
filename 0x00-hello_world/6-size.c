28299fcaa70dafe621cb3acb7429c4f6496472ec
SGXaba <sgxaba7@gmail.com>
2022-12-08 13:48:48 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
  * main - Entry
- * Return: Always 0
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -11,4 +12,5 @@ int main(void)
 	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %i byte(s)\n", sizeof(float);
 		return (0);
+
 }
