b29fb723753ee331514aebb2af57e0807ef76710
SGXaba <sgxaba7@gmail.com>
2022-12-17 07:54:13 -0800

    main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
-void postitive_or_negative(int 1);
+void postitive_or_negative(int i);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 
