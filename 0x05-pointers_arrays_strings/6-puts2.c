7aad1baaa4028b95f01581a6ebde62ad7d1a653e
SGXaba <sgxaba7@gmail.com>
2022-12-20 09:22:44 -0800

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
-* puts2 - prints very other character in string.
-* @str: pointer to string.
-*
-* Return: void.
-*/
+ * puts - prints very other character in string.
+ * @str: pointer to string.
+ *
+ * Return: void.
+ */
 void puts2(char *str)
 {
 	int i;
