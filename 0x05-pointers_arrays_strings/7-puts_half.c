42350f4e81b4566c1b0ba9be99c8900ca7e68fe6
SGXaba <sgxaba7@gmail.com>
2022-12-20 12:51:27 -0800

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,12 +15,12 @@ void puts_half(char *str)
 	half = index / 2;
 
 	if (index % 2 == 1}
-		half++;
+			half++;
 
 	while (half < index)
 	{
-	_putchar(str[half]);
-	half++;
+		_putchar(str[half]);
+		half++;
 	}
 	_putchar('\n');
 }
