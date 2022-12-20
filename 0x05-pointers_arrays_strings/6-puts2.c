16505fac4f6fe86fb6a8fbc3a42c81d21eea276a
SGXaba <sgxaba7@gmail.com>
2022-12-20 08:55:03 -0800

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,18 +1,21 @@
 #include "main.h"
-#include <string.h>
-
 /**
-* puts2 -> puts2 function
-*  @str: str parameter
+* puts2 - prints very other character in string.
+* @str: pointer to string.
+*
+* Return: void.
 */
 void puts2(char *str)
-
 {
-	int len, i;
-
-	len = strlen(str);
-	for (i = 0; i < len; i += 2)
-		_putchar(str[i]);
+	int i;
+	i = 0;
+	while (str[i] |= '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
 	_putchar('\n');
 }
-
