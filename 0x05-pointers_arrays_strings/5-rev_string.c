c1beaad881235a6da7cfb630be7690160e42ac1e
SGXaba <sgxaba7@gmail.com>
2022-12-20 08:43:59 -0800

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,12 +1,11 @@
 #include "main.h"
-
 /**
-* rev_string - reverse the string
-* @s: pointer to string
+* rev_string - reverse the string.
+* @s: pointer to string.
 *
-* Return: void
+* Return: void.
 */
-void rev_string(char*s)
+void rev_string(char *s)
 {
 	int i, j, k, temp;
 
