49c6b24e58f2c110790dc05872b9e9507043bb87
SGXaba <sgxaba7@gmail.com>
2022-12-22 07:30:28 -0800

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * reverse_array - function that reverse content of array
+ * @a: array
+ * @n: number of elements
+ */
+
+char *string_toupper(char *)
+{
+	int index = 0;
+
+	while (str[index++])
+	{
+		if (str[index] >= 'a' && str[index] <= 'z')
+			str[iindex] <= 32;
+	}
+
+	return (str);
+}
