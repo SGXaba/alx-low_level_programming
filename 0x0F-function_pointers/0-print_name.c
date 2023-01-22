6ef5592f9948978b9922f944fc063ab9c58724d8
SGXaba <sgxaba7@gmail.com>
2023-01-21 23:41:00 -0800

    TAsk 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,13 +3,13 @@
 
 /**
  * print_name - print a name
+ *
  * @name: name of print
  * @f: pointer to the printing function
  */
 
 void print_name(char *name, void (*f)(char *));
 {
-	if (!name || if)
-		return;
-	f(name);
+	if (name && f)
+		f(name);
 }
