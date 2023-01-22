0c729d396f13e9f6de0188b38a264df93cdaaaa2
SGXaba <sgxaba7@gmail.com>
2023-01-22 00:33:36 -0800

    Task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * array_iterator -calls a function to on arry lements
+ * @array: NAe ofthe array in which the function acts on.
+ * @size: size of the passed array to function
+ * @action: the function that acts on the array elements
+ * Author: SGXaba
+ * Return: no return because we've declared VOID
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (!array || !action)
+		return;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
