29edfd3426854ae41611591491dba7194cc890e4
SGXaba <sgxaba7@gmail.com>
2023-01-28 10:10:01 -0800

    Static libraries

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncat - function that concatnates two strings
+ * @dest: copy to
+ * @src: copy from
+ * @n: number of char to be copied
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j
+		dest[i] = src[j];
+		i++;
+	}
+	dest[i] = '\0'
+}
